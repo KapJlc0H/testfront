fc63f6b49b79e4c25804db1d8f634eb4591c850e
Marat <drug174@yandex.ru>
2020-10-27 16:23:57 +0300

    first try

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Великое древо навыков Марата</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/favicon.ico" type="image">
+    <link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+<header class="header">
+    <a href="http://d99902j0.beget.tech/#"><img src="img/logo.jpg" alt="logo"></a>
+        <nav class="menu">
+            <a class="menu_link menu_link_active" href="http://d99902j0.beget.tech">Главная</a>
+            <a class="menu_link" href="sheets/examp.html">Примеры</a>
+            <a class="menu_link" href="sheets/photo.html">Хобби</a>
+        </nav>
+</header>
+<section class="top">
+    <div class="top-content">
+    <h1 class="top-content_heading">Добро пожаловать в мою скромную обитель</h1>
+        <p class="top-content_text">Именно здесь я попытаюсь ознакомить всех заинтересованных своими навыками</p>
+        <!-- <a class="button" href="#">Обновить страницу</a>-->
+        </div>
+    <img  class="top-img"  src="img/top_img.jpg" alt="top img">
+</section>
+<section class="services">
+<h2 class="heading">Основные навыки</h2>
+<div class="service_content">
+    <div class="services_item">
+        <img src="img/Frame1.jpg" alt="ph">
+        <h3 class="services_h3">HoReCa-инженер</h3>
+        <p class="services_text">Имею 10-летний опыт в данной сфере. Работал с дверными терминалами(программируемые) в гостинице, POS-терминалы, фискальники, R-keeper, IIKO в самой крупной сети ресторанов ~450 ресторанов. Также знаком с ПО для систем боулинга и кинотеатров.</p>
+    </div>
+    <div class="services_item">
+        <img src="img/Frame2.jpg" alt="ph">
+        <h3 class="services_h3">Сисадмин</h3>
+        <p class="services_text">Разворачивал сервера, бэкапы, работал с СКС, занимался АТС, немного видеонаблюдение, шлагбаумы и даже пожарка. Могу кабель обжать и коаксиал воткнуть.</p>
+    </div>
+    <div class="services_item">
+        <img src="img/Frame3.jpg" alt="ph">
+        <h3 class="services_h3">Хэлпдеск</h3>
+        <p class="services_text">Что тут греха таить? Помогал и помогаю людям! Решаю проблемы пользователей эффективно и навсегда.</p>
+    </div>
+    <div class="services_item">
+        <img src="img/Frame4.jpg" alt="ph">
+        <h3 class="services_h3">Базы данных</h3>
+        <p class="services_text">Писать простенькие запросы для выполнения поиска или создания. Знаком с select, join, update, create. Могу установить SQL и удалить запросто тоже могу.</p>
+    </div>
+    <div class="services_item">
+        <img src="img/Frame5.jpg" alt="ph">
+        <h3 class="services_h3">Руководство людьми</h3>
+        <p class="services_text">Был руководителем группы выездных и удаленных инженеров. Составлял расписание для них, премирование, штрафы, выполнение работ. Штат ~20 человек.</p>
+    </div>
+    <div class="services_item">
+        <img src="img/Frame6.jpg" alt="ph">
+        <h3 class="services_h3">Дотошность</h3>
+        <p class="services_text">Изучаю нужные мне системы, писал инструкции для 1ой и 2ой линии поддержки. Искал решение сложных проблем и документировал это. Считаю это важным аспектом работы.</p>
+    </div>
+</div>
+</section>
+<section class="info">
+    <img class="image" src="img/trafsec2.jpg" alt="photo">
+    <div class="info_content">
+        <h3 class="heading heading_left">Что я хочу с таким багажом знаний?</h3>
+        <p class="text">А хочется мне банального, хочу гореть своим делом, заниматься тем, что мне интересно! Изучаю сейчас QA, frontend, немного управления продуктом. Интересно было бы работать в области кино или Gamedev.
+        Люблю изучать!</p>
+        <!--<a href="#" class="button">Learn more</a>-->
+    </div>
+</section>
+<section class="info">
+        <div class="info_content">
+        <h3 class="heading heading_left">Резюме</h3>
+        <p class="text">Тут кнопка, которая откроет вам в новой вкладке мое дополнительное резюме. Основное могу предоставить по запросу.</p>
+        <a href="https://spb.hh.ru/resume/fd38ea63ff07f6a18d0039ed1f5a4341445046" target="_blank" class="button"> Открыть резюме <img src="img/vector.svg" alt=""></a>
+    </div>
+    <img class="image" src="img/trafsec3.jpg" alt="photo">
+</section>
+</div>
+<footer class="footer">На коленке запилено. Все такое. 2020.</footer>
+</body>
+</html>
---
+++ project1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Фронтенд такой фронтенд</title>
+  <META Name= Author Lang="ru" content="Кадыров Марат">
+
+
+  <link rel="stylesheet" href="mystyle.css" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+
+</head>
+
+<body>
+
+<header>
+  <input type="checkbox" id="switcher">
+  <label class="open" for="switcher"><i class="fas fa-bars"></i></label>
+  <nav>
+  <ul class="menu">
+    <li> <a class ="menu_link" href="#"> Мой</a> </li>
+    <li> <a class ="menu_link" href="#"> Многострадальный</a> </li>
+    <li> <a class ="menu_link" href="#"> Frontend</a> </li>
+    <li> <a class ="menu_link" href="#"> !</a> </li>
+  </ul>
+
+  <label class="close" for="switcher"><i class="fas fa-window-close"></i></label>
+  </nav>
+</header>
+</body>
+</html>
